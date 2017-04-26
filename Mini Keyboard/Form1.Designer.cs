ab3902067237e15c69539af93889588d822842a0
OliverBiggins <obiggins@me.com>
2017-04-26 22:02:56 +0100

    Design 4
    Added button colours

--- Mini Keyboard/Form1.Designer.cs
+++ Mini Keyboard/Form1.Designer.cs
@@ -106,172 +106,191 @@
             // 
             // btn_Mode
             // 
+            this.btn_Mode.BackColor = System.Drawing.SystemColors.Highlight;
             this.btn_Mode.Location = new System.Drawing.Point(199, 219);
             this.btn_Mode.Name = "btn_Mode";
             this.btn_Mode.Size = new System.Drawing.Size(50, 50);
             this.btn_Mode.TabIndex = 4;
             this.btn_Mode.Text = "Mode";
-            this.btn_Mode.UseVisualStyleBackColor = true;
+            this.btn_Mode.UseVisualStyleBackColor = false;
             // 
             // btn_Next
             // 
+            this.btn_Next.BackColor = System.Drawing.SystemColors.Highlight;
             this.btn_Next.Location = new System.Drawing.Point(255, 220);
             this.btn_Next.Name = "btn_Next";
             this.btn_Next.Size = new System.Drawing.Size(50, 50);
             this.btn_Next.TabIndex = 5;
             this.btn_Next.Text = "Next";
-            this.btn_Next.UseVisualStyleBackColor = true;
+            this.btn_Next.UseVisualStyleBackColor = false;
             // 
             // btn_Prev
             // 
+            this.btn_Prev.BackColor = System.Drawing.SystemColors.Highlight;
             this.btn_Prev.Location = new System.Drawing.Point(311, 219);
             this.btn_Prev.Name = "btn_Prev";
             this.btn_Prev.Size = new System.Drawing.Size(50, 50);
             this.btn_Prev.TabIndex = 6;
             this.btn_Prev.Text = "Previo-us";
-            this.btn_Prev.UseVisualStyleBackColor = true;
+            this.btn_Prev.UseVisualStyleBackColor = false;
             // 
             // btn_7
             // 
+            this.btn_7.BackColor = System.Drawing.Color.Red;
             this.btn_7.Location = new System.Drawing.Point(199, 275);
             this.btn_7.Name = "btn_7";
             this.btn_7.Size = new System.Drawing.Size(50, 50);
             this.btn_7.TabIndex = 7;
             this.btn_7.Text = "7\r\n.,\"";
-            this.btn_7.UseVisualStyleBackColor = true;
+            this.btn_7.UseVisualStyleBackColor = false;
             // 
             // btn_8
             // 
+            this.btn_8.BackColor = System.Drawing.Color.Red;
             this.btn_8.Location = new System.Drawing.Point(255, 275);
             this.btn_8.Name = "btn_8";
             this.btn_8.Size = new System.Drawing.Size(50, 50);
             this.btn_8.TabIndex = 8;
             this.btn_8.Text = "8\r\nABC";
-            this.btn_8.UseVisualStyleBackColor = true;
+            this.btn_8.UseVisualStyleBackColor = false;
             // 
             // btn_9
             // 
+            this.btn_9.BackColor = System.Drawing.Color.Red;
             this.btn_9.Location = new System.Drawing.Point(311, 275);
             this.btn_9.Name = "btn_9";
             this.btn_9.Size = new System.Drawing.Size(50, 50);
             this.btn_9.TabIndex = 9;
             this.btn_9.Text = "9\r\nDEF";
-            this.btn_9.UseVisualStyleBackColor = true;
+            this.btn_9.UseVisualStyleBackColor = false;
             // 
             // btn_4
             // 
+            this.btn_4.BackColor = System.Drawing.Color.Red;
             this.btn_4.Location = new System.Drawing.Point(199, 331);
             this.btn_4.Name = "btn_4";
             this.btn_4.Size = new System.Drawing.Size(50, 50);
             this.btn_4.TabIndex = 10;
             this.btn_4.Text = "4\r\nGHI";
-            this.btn_4.UseVisualStyleBackColor = true;
+            this.btn_4.UseVisualStyleBackColor = false;
             // 
             // btn_5
             // 
+            this.btn_5.BackColor = System.Drawing.Color.Red;
             this.btn_5.Location = new System.Drawing.Point(255, 331);
             this.btn_5.Name = "btn_5";
             this.btn_5.Size = new System.Drawing.Size(50, 50);
             this.btn_5.TabIndex = 11;
             this.btn_5.Text = "5\r\nJKL";
-            this.btn_5.UseVisualStyleBackColor = true;
+            this.btn_5.UseVisualStyleBackColor = false;
             // 
             // btn_6
             // 
+            this.btn_6.BackColor = System.Drawing.Color.Red;
             this.btn_6.Location = new System.Drawing.Point(311, 331);
             this.btn_6.Name = "btn_6";
             this.btn_6.Size = new System.Drawing.Size(50, 50);
             this.btn_6.TabIndex = 12;
             this.btn_6.Text = "6\r\nMNO";
-            this.btn_6.UseVisualStyleBackColor = true;
+            this.btn_6.UseVisualStyleBackColor = false;
             // 
             // btn_1
             // 
+            this.btn_1.BackColor = System.Drawing.Color.Red;
             this.btn_1.Location = new System.Drawing.Point(199, 386);
             this.btn_1.Name = "btn_1";
             this.btn_1.Size = new System.Drawing.Size(50, 50);
             this.btn_1.TabIndex = 13;
             this.btn_1.Text = "1\r\nPQRS";
-            this.btn_1.UseVisualStyleBackColor = true;
+            this.btn_1.UseVisualStyleBackColor = false;
             // 
             // btn_2
             // 
+            this.btn_2.BackColor = System.Drawing.Color.Red;
             this.btn_2.Location = new System.Drawing.Point(255, 387);
             this.btn_2.Name = "btn_2";
             this.btn_2.Size = new System.Drawing.Size(50, 50);
             this.btn_2.TabIndex = 14;
             this.btn_2.Text = "2\r\nTUV";
-            this.btn_2.UseVisualStyleBackColor = true;
+            this.btn_2.UseVisualStyleBackColor = false;
             // 
             // btn_3
             // 
+            this.btn_3.BackColor = System.Drawing.Color.Red;
             this.btn_3.Location = new System.Drawing.Point(311, 387);
             this.btn_3.Name = "btn_3";
             this.btn_3.Size = new System.Drawing.Size(50, 50);
             this.btn_3.TabIndex = 15;
             this.btn_3.Text = "3\r\nWXYZ";
-            this.btn_3.UseVisualStyleBackColor = true;
+            this.btn_3.UseVisualStyleBackColor = false;
             // 
             // btn_Punc
             // 
+            this.btn_Punc.BackColor = System.Drawing.Color.Red;
             this.btn_Punc.Location = new System.Drawing.Point(199, 442);
             this.btn_Punc.Name = "btn_Punc";
             this.btn_Punc.Size = new System.Drawing.Size(50, 50);
             this.btn_Punc.TabIndex = 16;
             this.btn_Punc.Text = "\"-...";
-            this.btn_Punc.UseVisualStyleBackColor = true;
+            this.btn_Punc.UseVisualStyleBackColor = false;
             // 
             // btn_Space
             // 
+            this.btn_Space.BackColor = System.Drawing.Color.LawnGreen;
             this.btn_Space.Location = new System.Drawing.Point(255, 442);
             this.btn_Space.Name = "btn_Space";
             this.btn_Space.Size = new System.Drawing.Size(50, 50);
             this.btn_Space.TabIndex = 17;
             this.btn_Space.Text = "000\r\n[ _ ]";
-            this.btn_Space.UseVisualStyleBackColor = true;
+            this.btn_Space.UseVisualStyleBackColor = false;
             // 
             // btn_0
             // 
+            this.btn_0.BackColor = System.Drawing.Color.Red;
             this.btn_0.Location = new System.Drawing.Point(311, 442);
             this.btn_0.Name = "btn_0";
             this.btn_0.Size = new System.Drawing.Size(50, 50);
             this.btn_0.TabIndex = 18;
             this.btn_0.Text = "#\r\n000";
-            this.btn_0.UseVisualStyleBackColor = true;
+            this.btn_0.UseVisualStyleBackColor = false;
             // 
             // btn_SendPredict
             // 
+            this.btn_SendPredict.BackColor = System.Drawing.Color.Orange;
             this.btn_SendPredict.Location = new System.Drawing.Point(367, 220);
             this.btn_SendPredict.Name = "btn_SendPredict";
             this.btn_SendPredict.Size = new System.Drawing.Size(50, 50);
             this.btn_SendPredict.TabIndex = 19;
             this.btn_SendPredict.Text = "Send Predic-ted";
-            this.btn_SendPredict.UseVisualStyleBackColor = true;
+            this.btn_SendPredict.UseVisualStyleBackColor = false;
             // 
             // btn_Blank
             // 
+            this.btn_Blank.BackColor = System.Drawing.Color.Orange;
             this.btn_Blank.Location = new System.Drawing.Point(367, 276);
             this.btn_Blank.Name = "btn_Blank";
             this.btn_Blank.Size = new System.Drawing.Size(50, 50);
             this.btn_Blank.TabIndex = 20;
-            this.btn_Blank.UseVisualStyleBackColor = true;
+            this.btn_Blank.UseVisualStyleBackColor = false;
             // 
             // btn_Blank2
             // 
+            this.btn_Blank2.BackColor = System.Drawing.Color.Orange;
             this.btn_Blank2.Location = new System.Drawing.Point(367, 331);
             this.btn_Blank2.Name = "btn_Blank2";
             this.btn_Blank2.Size = new System.Drawing.Size(50, 50);
             this.btn_Blank2.TabIndex = 21;
-            this.btn_Blank2.UseVisualStyleBackColor = true;
+            this.btn_Blank2.UseVisualStyleBackColor = false;
             // 
             // btn_Enter
             // 
+            this.btn_Enter.BackColor = System.Drawing.Color.Orange;
             this.btn_Enter.Location = new System.Drawing.Point(367, 386);
             this.btn_Enter.Name = "btn_Enter";
             this.btn_Enter.Size = new System.Drawing.Size(50, 106);
             this.btn_Enter.TabIndex = 22;
             this.btn_Enter.Text = "Enter";
-            this.btn_Enter.UseVisualStyleBackColor = true;
+            this.btn_Enter.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
