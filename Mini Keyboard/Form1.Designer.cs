86c8b8ee48e2f73344e8c44328c80967c95b0913
OliverBiggins <obiggins@me.com>
2017-04-27 16:01:46 +0100

    Current Letter
    Added a current letter viewer

--- Mini Keyboard/Form1.Designer.cs
+++ Mini Keyboard/Form1.Designer.cs
@@ -54,6 +54,7 @@
             this.btn_Blank2 = new System.Windows.Forms.Button();
             this.btn_Enter = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.txt_CurrentLetter = new System.Windows.Forms.TextBox();
             this.menuStrip.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -93,13 +94,13 @@
             // 
             this.txt_WordBuilder.Location = new System.Drawing.Point(199, 184);
             this.txt_WordBuilder.Name = "txt_WordBuilder";
-            this.txt_WordBuilder.Size = new System.Drawing.Size(151, 30);
+            this.txt_WordBuilder.Size = new System.Drawing.Size(128, 30);
             this.txt_WordBuilder.TabIndex = 2;
             this.txt_WordBuilder.Text = "";
             // 
             // txt_ModeStatus
             // 
-            this.txt_ModeStatus.Location = new System.Drawing.Point(356, 187);
+            this.txt_ModeStatus.Location = new System.Drawing.Point(367, 191);
             this.txt_ModeStatus.Name = "txt_ModeStatus";
             this.txt_ModeStatus.ReadOnly = true;
             this.txt_ModeStatus.Size = new System.Drawing.Size(64, 20);
@@ -318,11 +319,20 @@
             this.label1.TabIndex = 23;
             this.label1.Text = "Press Next to move onto next letter";
             // 
+            // txt_CurrentLetter
+            // 
+            this.txt_CurrentLetter.Location = new System.Drawing.Point(332, 186);
+            this.txt_CurrentLetter.Multiline = true;
+            this.txt_CurrentLetter.Name = "txt_CurrentLetter";
+            this.txt_CurrentLetter.Size = new System.Drawing.Size(29, 28);
+            this.txt_CurrentLetter.TabIndex = 24;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(617, 518);
+            this.Controls.Add(this.txt_CurrentLetter);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btn_Enter);
             this.Controls.Add(this.btn_Blank2);
@@ -384,6 +394,7 @@
         private System.Windows.Forms.Button btn_Blank2;
         private System.Windows.Forms.Button btn_Enter;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_CurrentLetter;
 
     }
 }
